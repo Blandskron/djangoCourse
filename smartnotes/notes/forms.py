e498be3150b2c63612257794ab9c0b5a69eda83f
Blandskron <bastianlandskronfreelancer@gmail.com>
2023-10-26 23:57:27 -0300

    addanddeletenote

--- smartnotes/notes/forms.py
+++ smartnotes/notes/forms.py
@@ -14,10 +14,3 @@ class NotesForm(forms.ModelForm):
         labels = {
             'text': 'Write your thoughts here:'
         }
-
-    def clean_title(self):
-        title = self.cleaned_data['title']
-        if 'Django' not in title:
-            raise forms.ValidationError('We only accept notes about Django!')
-        return title
-        
