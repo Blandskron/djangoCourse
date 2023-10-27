92dcbcf01827e3725fec9faa589aa163a8f48550
Blandskron <bastianlandskronfreelancer@gmail.com>
2023-10-27 17:27:50 -0300

    addtemplates

---
+++ nandiasgarden/pizza/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+from .models import Pizza
+
+class PizzaForm(forms.Form):
+    toppings = forms.ModelChoiceField(choices=[('pep','Pepperoni'),('cheese','Cheese'),('olives','Olives')])
+    size = forms.ChoiceField(label='Size', choices=[('Small','Small'),('Medium','Medium'),('Large','Large')])
+
+# class PizzaForm(forms.ModelForm):
+#     class Meta:
+#         model = Pizza
+#         fields = ['topping1','topping2','size']
+#         labels = {'topping1':'Topping 1','topping2':'Topping 2'}
