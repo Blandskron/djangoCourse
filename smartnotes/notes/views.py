b5ab962517a6391cbf2d838b6c8a20e508fe48e9
Blandskron <bastianlandskronfreelancer@gmail.com>
2023-10-26 22:17:34 -0300

    basetemplates

--- smartnotes/notes/views.py
+++ smartnotes/notes/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.http import Http404
+from django.views.generic import DetailView, ListView
 
-# Create your views here.
+from .models import Notes
+
+class NotesListView(ListView):
+    model = Notes
+    context_object_name = "notes"
+    template_name = "notes/notes_list.html"
+
+class NotesDetailview(DetailView):
+    model = Notes
+    context_object_name = "note"
+
+def detail(request, pk):
+    try:
+        note = Notes.objects.get(pk=pk)
+    except Notes.DoesNotExist:
+        raise Http404("Note does not exist")
+    return render(request, 'notes/notes_detail.html', {'note': note})
